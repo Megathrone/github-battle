0bbd9ce1ac60f458df95fa324f33ab49337beebd
Megathrone <yizhouwang418@gmail.com>
2020-03-28 16:59:21 -0400

    battle api

---
+++ app/components/Results.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {battle} from "../utils/api";
+
+export default class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const {playerOne, playerTwo} = this.props
+
+    battle([playerOne, playerTwo])
+    .then((players) => {
+      console.log('data:', players)
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          Results
+          <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        </div>
+    )
+  }
+
+}
