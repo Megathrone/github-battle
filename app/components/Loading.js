eef9180337c28df81aad1c5801f77ec39e84c6e0
Megathrone <yizhouwang418@gmail.com>
2020-04-01 21:23:16 -0400

    using vscode

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,52 +1,48 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 const styles = {
   content: {
-    fontSize: '35px',
-    position: 'absolute',
-    left: '0',
-    right: '0',
-    marginTop: '20px',
-    textAlign: 'center',
+    fontSize: "35px",
+    position: "absolute",
+    left: "0",
+    right: "0",
+    marginTop: "20px",
+    textAlign: "center"
   }
-}
+};
 
 export default class Loading extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       content: props.text
-    }
+    };
   }
-  componentDidMount () {
-    const { speed, text } = this.props
+  componentDidMount() {
+    const { speed, text } = this.props;
 
     this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-          ? this.setState({ content: text })
-          : this.setState(({ content }) => ({ content: content + '.' }))
-    }, speed)
+      this.state.content === text + "..."
+        ? this.setState({ content: text })
+        : this.setState(({ content }) => ({ content: content + "." }));
+    }, speed);
   }
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
   }
   render() {
-    return (
-        <p style={styles.content}>
-          {this.state.content}
-        </p>
-    )
+    return <p style={styles.content}>{this.state.content}</p>;
   }
 }
 
 Loading.propTypes = {
   text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired,
-}
+  speed: PropTypes.number.isRequired
+};
 
 Loading.defaultProps = {
-  text: 'Loading',
+  text: "Loading",
   speed: 300
-}
+};
